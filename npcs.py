20ba02fd1416a49f83d8425498de1114a10d83b9
Jevon <iheartbenzene@gmail.com>
2019-08-27 19:08:01 -0400

    also started the `npcs.py` file

--- npcs.py
+++ npcs.py
@@ -8,14 +8,41 @@ from os import random as truerand
 
 
 class Npc(object):
-    name = ""
-    health = 1
-    inventory = []
+    
+    __Coins = 10
+    __NpcCount = 0
     
     @staticmethod
     def vendor_greetings():
         print("Can't wait to count out your coin") # Credit to Skyrim
         
-
-
+    def __init__(self, name = ''):
+        self.__name = name
+        Npc.__SetCoins()
+        Npc.__SetNpcCount() 
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        self.__name = value
+    
+    def SetNpcName(self):
+        return self.name
+    
+    def __str__(self):
+        return self.name
+    
+    @staticmethod
+    def GetCoins():
+        return Npc.__Coins
+            
+    @staticmethod
+    def __SetCoins():
+        Npc.__Coins += 3
         
+    @staticmethod
+    def __SetNpcCount():
+        Npc.__NpcCount += 1
